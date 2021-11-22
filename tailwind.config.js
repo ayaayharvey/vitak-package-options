923fef6de5bce41f91c6834e8740a1b5d57713bf
harveyayaay <harveyayaay@gmail.com>
2021-11-23 07:26:03 +0800

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,23 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		},
 		extend: {
 			fontFamily: {
 				body: "'Montserrat', sans-serif"
 			},
 			colors: {
 				primary: '#41883E',
+				theme: {
+					green: '#41883E',
+					orange: '#E1591A'
+				},
 				gray: {
 					light: '#FAFAFA'
 				},
